bc5a9ce688ef34b24924aa6945586514814bc0ec
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-04-23 14:14:02 -0400

    setup Edge and MyGraph class

---
+++ src/main/java/org/example/Edge.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Edge {
+    int v1;
+    int v2;
+    int weight;
+
+    /**
+     * Constructor that sets all three member variables */
+    public Edge(int v1, int v2, int weight) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.weight = weight;
+    }
+
+}
---
+++ src/main/java/org/example/MyGraph.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyGraph {
+
+    List<Integer> vertices;
+
+
+
+
+
+
+
+}
